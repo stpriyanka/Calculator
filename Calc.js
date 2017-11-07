51cc6cd5ecf66508c828ec3a924e276a6183b64b
Priyanka S.Tasnia <stpriyanka2011@gmail.com>
2017-11-07 22:32:23 +0100

    Added css

--- Calc.js
+++ Calc.js
@@ -1,7 +1,14 @@
 function buttonClick(button)
 {
-	var val= button.value;
 	var tb3 = document.getElementById("print");
-	tb3.value=val;
+	
+	var val= button.value;
+	if(val == 'AC')
+	{
+		tb3.value='';
+	}else
+	{
+		tb3.value=val;
+	}
 }
 
